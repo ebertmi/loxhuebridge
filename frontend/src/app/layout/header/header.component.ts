2f3a99b42247fde332c9304bcf9627c37ff3c036
Michael <1634468+ebertmi@users.noreply.github.com>
2026-02-08 10:25:52 +0100

    Fixed some style issues

--- frontend/src/app/layout/header/header.component.ts
+++ frontend/src/app/layout/header/header.component.ts
@@ -15,7 +15,7 @@ import { ThemeService } from '../../core/services/theme.service';
         <img src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='%2385c440'%3E%3Cpath d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z'/%3E%3C/svg%3E"
              alt="Logo"
              class="logo" />
-        <span class="title">loxHueBridge</span>
+        <span class="title">Loxone-Hue-Bridge</span>
       </div>
       <span class="spacer"></span>
       <button mat-icon-button
